4546753f16d6bcc8c40729fe5d61f225dad37045
Arsh Mishra <arshm0312@gmail.com>
2025-09-10 23:26:26 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,12 @@ import bodyParser from "body-parser";
 import cors from "cors";
 
 
-const url = "mongodb://localhost:27017";
+const url = process.env.MONGO_URI;
 const client = new MongoClient(url);
 const dbName = "PasswordManager";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
